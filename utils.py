a218e435683b5c147c04c08f0678bb5e0185402c
Mihir Jain <48515930+mihircjain@users.noreply.github.com>
2020-02-13 18:53:30 -0800

    Updated utils.py

--- utils.py
+++ utils.py
@@ -1,31 +1,36 @@
-from dataloader import *
+import numpy as np
 def iou(pred, target):
-    pred = pred.cuda()
-    target = target.cuda()
-    ious_tp = []
-    ious_fp = []
-    ious_fn = []
-    #pred = pred.numpy(), 
-    for cls in range(34):
-        # Complete this function
-        
-
-        TP = np.sum(np.argwhere(pred==target==cls,1,0))
-        FP = np.sum(np.argwhere(pred==cls!=target,1,0))
-        FN = np.sum(np.argwhere(pred!=cls and target==cls,1,0))
-        intersection = TP # intersection calculation
-        union = TP + FP +FN # Union calculation
-        if union == 0:
-            ious_tp.append(float('nan'))
-            ious_fp.append(float('nan'))
-            ious_fn.append(float('nan'))
-            # if there is no ground truth, do not include in evaluation
-        else:
-            ious_tp.append(TP)
-            ious_fp.append(FP)
-            ious_fn.append(FN)
-                #ious.append(intersection/union)# Append the calculated IoU to the list ious
-    return ious_tp, ious_fp, ious_fn
+   
+    print(pred.shape)
+    pred = pred.cpu().detach().numpy()
+    target = target.cpu().detach().numpy()
+    bad_ids = [0,1,2,3,4,5,6,9,10,14,15,16,18, 29,30]
+    out = np.ones((len(pred), 34-len(bad_ids)))
+    number_of_images = int(len(pred))
+    n_class = 34
+    for i in range(number_of_images):        
+        j = 0        
+        for cls in range(n_class):          
+            if (cls not in bad_ids):       
+                p = np.argmax(pred[i], axis = 0)
+                t = np.argmax(target[i], axis = 0)
+                               
+                classy = np.ones(p.shape) * cls
+                TP = np.where((p==classy) & (p==t),1,0).sum()
+                FP = np.where((p==classy) & (p!=t),1,0).sum()
+                FN = np.where((p!=classy) & (classy==t),1,0).sum()
+                print(TP, FP, FN)
+                intersection = TP # intersection calculation
+                union = TP + FP + FN # Union calculation
+                if union == 0:
+                    out[i,j] = 0
+                    j= j+1
+                    # if there is no ground truth, do not include in evaluation
+                else:
+                    out[i,j] = intersection/union
+                    j = j+1
+                    #ious.append(intersection/union)# Append the calculated IoU to the list ious
+        return out
 
 
 def pixel_acc(pred, target):
